bf8c999b956eb5c35d91fc23c9e1db28a36e27c7
Meconer <mats.asson@gmail.com>
2025-02-15 17:58:31 +0100

    Day 1 completed

--- lib/day1/day1.ml
+++ lib/day1/day1.ml
@@ -2,4 +2,15 @@ open Core
 
 let file_name = "lib/day1/input.txt"
 let aoc_input = In_channel.read_all file_name
-let result_p1=0
+
+let lst = String.to_list aoc_input |> List.map ~f:(fun c -> int_of_char c - int_of_char '0')
+
+let rec loop acc =function 
+  | [] -> acc
+  | _::[] -> acc
+  | a::b::tail -> if a = b then loop (acc+a) (b::tail) else loop acc (b::tail)
+
+let res = loop 0 lst
+let res = if (List.hd_exn lst = List.last_exn lst) then res + List.hd_exn lst else res
+
+let result_p1=res
