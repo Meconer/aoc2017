80b1e19be15ddb08a4b6728cb5af77ad111c84c2
Meconer <mats.asson@gmail.com>
2025-02-16 21:15:11 +0100

    wip

--- lib/day3/day3.ml
+++ lib/day3/day3.ml
@@ -1,9 +1,15 @@
 let cell_no = 361527
 
-(* Turns at 1 2 3 5 7 10 13 17 21 26 31 37 43 50 57
-   diffs     1 1 2 2 3  3  4  4  5  5  6  6  7  7
-   sq diff   1 1 4 4 9  9 16 16 25 25 36 36 49 49
-   *)
+let numbers_straight limit start_diff =
+  let rec loop n diff =
+    if n > limit then n
+    else if n = 1 then loop (n + diff) (diff + 8)
+    else
+      let n = n + diff in
+      Printf.printf "%d\n" n;
+      loop n (diff + 8)
+  in
+  loop 1 start_diff
 
 let result_p1 = 0
 let result_p2 = 0
