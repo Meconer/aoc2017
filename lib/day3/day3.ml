016a2902d629f85c7d3f4ff9ea6221e1abb2054d
Meconer <mats.asson@gmail.com>
2025-02-17 08:46:44 +0100

    Day 3 part 1 complete

--- lib/day3/day3.ml
+++ lib/day3/day3.ml
@@ -1,15 +1,26 @@
-let cell_no = 361527
+let input_no = 361527
 
+(* 
+  Gets the numbers by going straight from the center (1 pos)
+  Use start_diff = 1 for going right. 3 for going up, 5 for
+  left and 7 for down 
+ *)
 let numbers_straight limit start_diff =
-  let rec loop n diff =
-    if n > limit then n
-    else if n = 1 then loop (n + diff) (diff + 8)
+  let rec loop n diff dist last =
+    if n > limit then last
     else
+      let last = (n, dist) in
       let n = n + diff in
-      Printf.printf "%d\n" n;
-      loop n (diff + 8)
+      loop n (diff + 8) (dist + 1) last
   in
-  loop 1 start_diff
+  loop 1 start_diff 0 (1, 0)
 
-let result_p1 = 0
+(* 
+  By trying my input I found out that 
+  I can get there by going right 301 steps and then
+    the rest upwards
+*)
+
+let top_number, dist = numbers_straight input_no 1
+let result_p1 = dist + input_no - top_number
 let result_p2 = 0
