c8f1790e143ee19ebe49bd2de4c106aaf0f6d90c
Meconer <mats.asson@gmail.com>
2025-02-21 14:05:41 +0100

    wip

--- lib/day9/day9.ml
+++ lib/day9/day9.ml
@@ -5,6 +5,19 @@ let is_example = false
 let filename =
   if is_example then "lib/day9/example.txt" else "lib/day9/input.txt"
 
-let aoc_input = In_channel.read_lines filename
+let aoc_input = In_channel.read_all filename
+
+let remove_cancelled s =
+  let lst = String.to_list s in
+  let rec rem_loop acc lst =
+    match lst with
+    | [] -> List.rev acc
+    | a :: [] -> if Char.( = ) a '!' then List.rev acc else List.rev (a :: acc)
+    | a :: b :: rest ->
+        if Char.( = ) a '!' then rem_loop acc rest
+        else rem_loop (a :: acc) (b :: rest)
+  in
+  String.of_list (rem_loop [] lst)
+
 let result_p1 = 0
 let result_p2 = 0
