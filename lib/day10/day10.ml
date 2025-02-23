5ee3e8af9e84c09baa4b43eda1ebc8e9944fe7d2
Meconer <mats.asson@gmail.com>
2025-02-23 15:43:17 +0100

    wip

--- lib/day10/day10.ml
+++ lib/day10/day10.ml
@@ -56,5 +56,26 @@ let unmoved_lst =
 
 let result_p1 = List.hd_exn unmoved_lst * List.nth_exn unmoved_lst 1
 let parse_input_p2 s = String.to_list s |> List.map ~f:int_of_char
-let l_seq_p2 = parse_input_p2 aoc_input
+let e_seq = [ 17; 31; 73; 47; 23 ]
+let l_seq_p2 = parse_input_p2 aoc_input @ e_seq |> Array.of_list
+
+let solve_p2 l_seq n_arr =
+  let rec loop n_arr skip_size curr_idx l_seq =
+    Printf.printf "Skip_size %d\n" skip_size;
+    match l_seq with 
+    | [] -> n_arr
+    | move_len::rest ->
+      let rec rev_loop idx cnt =
+        if cnt = 0 then ()
+        else
+          let i1 = idx in
+          let i2 = move_len - idx in
+          let v1 = Array.get n_arr i1 in
+          let v2 = Array.get n_arr i2 in
+          Array.set n_arr i1 v2;
+          Array.set n_arr i2 v1;
+
+  in
+  loop n_arr 0 0 l_seq
+
 let result_p2 = 0
