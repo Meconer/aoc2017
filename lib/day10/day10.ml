bbad08d40eb73a5db5157f9061305e1ce5b62ee3
Meconer <mats.asson@gmail.com>
2025-02-23 17:52:07 +0100

    wip

--- lib/day10/day10.ml
+++ lib/day10/day10.ml
@@ -57,25 +57,32 @@ let unmoved_lst =
 let result_p1 = List.hd_exn unmoved_lst * List.nth_exn unmoved_lst 1
 let parse_input_p2 s = String.to_list s |> List.map ~f:int_of_char
 let e_seq = [ 17; 31; 73; 47; 23 ]
-let l_seq_p2 = parse_input_p2 aoc_input @ e_seq |> Array.of_list
+let l_seq_p2 = parse_input_p2 aoc_input @ e_seq
+let n_arr = Array.init 256 ~f:(fun i -> i)
 
 let solve_p2 l_seq n_arr =
-  let rec loop n_arr skip_size curr_idx l_seq =
+  let rec loop skip_size curr_idx l_seq =
     Printf.printf "Skip_size %d\n" skip_size;
-    match l_seq with 
+    match l_seq with
     | [] -> n_arr
-    | move_len::rest ->
-      let rec rev_loop idx cnt =
-        if cnt = 0 then ()
-        else
-          let i1 = idx in
-          let i2 = move_len - idx in
-          let v1 = Array.get n_arr i1 in
-          let v2 = Array.get n_arr i2 in
-          Array.set n_arr i1 v2;
-          Array.set n_arr i2 v1;
-
+    | move_len :: rest ->
+        let rec rev_loop idx cnt =
+          if cnt = 0 then ()
+          else
+            let i1 = idx mod 256 in
+            let i2 = (move_len - idx) mod 256 in
+            let v1 = Array.get n_arr i1 in
+            let v2 = Array.get n_arr i2 in
+            Array.set n_arr i1 v2;
+            Array.set n_arr i2 v1;
+            rev_loop ((idx + 1) mod 256) (cnt - 1)
+        in
+        let _ = rev_loop curr_idx (move_len / 2) in
+        loop ((skip_size + 1) mod 256) ((curr_idx + move_len) mod 256) rest
   in
-  loop n_arr 0 0 l_seq
 
+  loop 0 0 l_seq
+
+let test_arr = Array.init 5 ~f:Fn.id
+let test_seq = [ 3; 4; 1; 5 ]
 let result_p2 = 0
