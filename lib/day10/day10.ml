d00b1e06bec6360c8bbc8621db097ea5e7caae76
Meconer <mats.asson@gmail.com>
2025-02-24 20:30:13 +0100

    WIP

--- lib/day10/day10.ml
+++ lib/day10/day10.ml
@@ -14,19 +14,17 @@ let parse_input s =
 
 let l_seq = parse_input aoc_input
 let parse_input_p2 s = String.to_list s |> List.map ~f:int_of_char
-let e_seq = [ 17; 31; 73; 47; 23 ]
-let l_seq_p2 = parse_input_p2 aoc_input @ e_seq
 let n_arr = Array.init 256 ~f:(fun i -> i)
 
 let print_arr arr =
   Array.iter arr ~f:(fun el -> Printf.printf "%d," el);
   Printf.printf "\n"
 
-let solve_p1 l_seq n_arr =
+let calc_knot l_seq n_arr skip_size start_idx =
   let arr_len = Array.length n_arr in
   let rec loop skip_size curr_idx l_seq =
     match l_seq with
-    | [] -> n_arr
+    | [] -> (n_arr, skip_size, curr_idx)
     | move_len :: rest ->
         let rec rev_loop idx cnt =
           if cnt = 0 then ()
@@ -44,11 +42,48 @@ let solve_p1 l_seq n_arr =
           ((curr_idx + move_len + skip_size) mod arr_len)
           rest
   in
+  loop skip_size start_idx l_seq
 
-  loop 0 0 l_seq
+let solve_p1 () =
+  let l_seq = parse_input aoc_input in
+  let n_arr = Array.init 256 ~f:(fun i -> i) in
+  let p1_arr, _, _ = calc_knot l_seq n_arr 0 0 in
+  p1_arr
 
-let p1_arr = solve_p1 l_seq n_arr
+let p1_arr = solve_p1 ()
 let result_p1 = Array.get p1_arr 0 * Array.get p1_arr 1
-let test_arr = Array.init 5 ~f:Fn.id
-let test_seq = [ 3; 4; 1; 5 ]
+let e_seq = [ 17; 31; 73; 47; 23 ]
+let l_seq_p2 = parse_input_p2 aoc_input @ e_seq
+
+let calc_sparse_hash l_seq_p2 =
+  let n_arr = Array.init 256 ~f:(fun i -> i) in
+  let rec loop skip_size curr_idx count n_arr =
+    if count = 0 then n_arr
+    else
+      let n_arr, skip_size, curr_idx =
+        calc_knot l_seq_p2 n_arr skip_size curr_idx
+      in
+      loop skip_size curr_idx (count - 1) n_arr
+  in
+  loop 0 0 64 n_arr
+
+let hex_of_int n = Printf.sprintf "%02x" n
+let sparse_hash = calc_sparse_hash l_seq_p2
+let combine lst = List.fold lst ~init:0 ~f:(fun acc el -> acc lxor el)
+
+let calc_dense_hash sparse_hash =
+  let sp_hash_list = Array.to_list sparse_hash in
+  let rec loop acc lst =
+    match lst with
+    | [] -> List.rev acc
+    | lst ->
+        let w_list = List.take lst 16 in
+        let r_list = List.drop lst 16 in
+        let hex = combine w_list in
+        loop (hex :: acc) r_list
+  in
+  let hex_list = loop [] sp_hash_list in
+  List.fold hex_list ~init:"" ~f:(fun acc n -> acc ^ hex_of_int n)
+
+let test_seq = parse_input_p2 ""
 let result_p2 = 0
