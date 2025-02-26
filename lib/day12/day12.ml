c2e60aae16be76b557b027cc91785609be0c2144
Meconer <mats.asson@gmail.com>
2025-02-26 21:01:19 +0100

    WIP

--- lib/day12/day12.ml
+++ lib/day12/day12.ml
@@ -5,10 +5,6 @@ let is_example = false
 let filename = "lib/day12/input.txt"
 let aoc_input = In_channel.read_lines filename
 
-let parse_line s =
-  let n1, conns = Scanf.sscanf s "%d <-> %s" (fun n s -> (n, s)) in
-  (n1, conns)
-
 let regex =
   compile
     (seq
@@ -32,24 +28,6 @@ let parse_line line =
       in
       Some (id, neighbors)
   | None -> None
-
-let () =
-  let test_lines =
-    [
-      "0 <-> 412, 480, 777, 1453";
-      "1 <-> 23";
-      "12 <-> 99, 200, 4500";
-      "5 <-> 8, 9";
-      "Invalid line";
-    ]
-  in
-
-  List.iter test_lines ~f:(fun line ->
-      match parse_line line with
-      | Some (id, neighbors) ->
-          printf "ID: %d, Neighbors: %s\n" id
-            (String.concat ~sep:", " (List.map ~f:Int.to_string neighbors))
-      | None -> printf "No match for: %s\n" line)
 
 let result_p1 = 0
 let result_p2 = 0
