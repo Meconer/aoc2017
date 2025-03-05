32593fee3be08b22e51da45d594c0d9c76f19c62
Meconer <mats.asson@gmail.com>
2025-03-05 18:44:35 +0100

    Day 17 p1

--- lib/day17/day17.ml
+++ lib/day17/day17.ml
@@ -1,10 +1,25 @@
-let is_example = false
+open Core
+
+let is_example = true
 let fw_jump = if is_example then 3 else 343
 
-let solve_p1 () = 
-  let circular_array = Array.make 2018 0 in
+let solve_p1 () =
+  let circular_array = Array.create ~len:2018 0 in
   let current_size = ref 1 in
   let current_pos = ref 0 in
+  let rec loop i =
+    if i = 2018 then ()
+    else (
+      current_pos := ((!current_pos + fw_jump) mod !current_size) + 1;
+      for j = !current_size downto !current_pos + 1 do
+        circular_array.(j) <- circular_array.(j - 1)
+      done;
+      circular_array.(!current_pos) <- i;
+      incr current_size;
+      loop (i + 1))
+  in
+  loop 1;
+  circular_array.(!current_pos + 1)
 
 let result_p1 = 0
 let result_p2 = 0
