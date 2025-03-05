f69c8545898f603e27c43bff67b5dc0fb0f4d74f
Meconer <mats.asson@gmail.com>
2025-03-05 16:45:21 +0100

    Day 17 started

---
+++ lib/day17/day17.ml
@@ -0,0 +1,7 @@
+let is_example = false
+let fw_jump = if is_example then 3 else 343
+let circular_array = Array.make 2018 0
+let current_size = ref 1
+let current_pos = ref 0
+let result_p1 = 0
+let result_p2 = 0
