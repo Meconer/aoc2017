c35fb0c92fb1e87ac9bfe91c01b584fe14b2f0d9
Meconer <mats.asson@gmail.com>
2025-03-05 17:00:35 +0100

    wip

--- lib/day17/day17.ml
+++ lib/day17/day17.ml
@@ -1,7 +1,10 @@
 let is_example = false
 let fw_jump = if is_example then 3 else 343
-let circular_array = Array.make 2018 0
-let current_size = ref 1
-let current_pos = ref 0
+
+let solve_p1 () = 
+  let circular_array = Array.make 2018 0 in
+  let current_size = ref 1 in
+  let current_pos = ref 0 in
+
 let result_p1 = 0
 let result_p2 = 0
