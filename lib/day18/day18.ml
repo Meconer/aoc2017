448a91b168cbfae9fba92826dab6f2e7e51461c7
Meconer <mats.asson@gmail.com>
2025-03-07 19:52:20 +0100

    wip

--- lib/day18/day18.ml
+++ lib/day18/day18.ml
@@ -9,12 +9,17 @@ type instr_t = Snd | Set | Add | Mul | Mod | Rcv | Jgz
 type arg = Reg of string | Val of int
 type instr = { instr : instr_t; arg1 : string; arg2 : arg option }
 
+let string_of_instr instr =
+  match instr.instr with Snd -> "snd" | Set -> "set" | Add -> "add"
+
 let registers = ref (Hashtbl.create (module String))
+let latest_sound = ref 0
 
 let get_arg arg =
   match arg with
-  | Reg r -> Hashtbl.find_or_add !registers r ~default:(fun () -> 0)
-  | Val v -> v
+  | Some (Reg r) -> Hashtbl.find_or_add !registers r ~default:(fun () -> 0)
+  | Some (Val v) -> v
+  | None -> failwith "Should be a second arg here"
 
 let set_arg arg value =
   match arg with
@@ -51,10 +56,76 @@ let parse_instr line =
 
 let aoc_input = In_channel.read_lines filename
 
+let do_set instr =
+  Hashtbl.set !registers ~key:instr.arg1 ~data:(get_arg instr.arg2)
+
+let do_add instr =
+  let curr_val =
+    Hashtbl.find_or_add !registers instr.arg1 ~default:(fun _ -> 0)
+  in
+  Hashtbl.set !registers ~key:instr.arg1 ~data:(curr_val + get_arg instr.arg2)
+
+let do_mul instr =
+  let curr_val =
+    Hashtbl.find_or_add !registers instr.arg1 ~default:(fun _ -> 0)
+  in
+  Hashtbl.set !registers ~key:instr.arg1 ~data:(curr_val * get_arg instr.arg2)
+
+let do_snd instr = latest_sound := Hashtbl.find_exn !registers instr.arg1
+
+let do_mod instr =
+  let curr_val =
+    Hashtbl.find_or_add !registers instr.arg1 ~default:(fun _ -> 0)
+  in
+  Hashtbl.set !registers ~key:instr.arg1 ~data:(curr_val mod get_arg instr.arg2)
+
+let do_rcv instr =
+  let curr_val =
+    Hashtbl.find_or_add !registers instr.arg1 ~default:(fun _ -> 0)
+  in
+  if curr_val = 0 then None else Some !latest_sound
+
+let do_jgz instr =
+  let curr_val =
+    Hashtbl.find_or_add !registers instr.arg1 ~default:(fun _ -> 0)
+  in
+  if curr_val = 0 then None else Some (get_arg instr.arg2)
+
 let do_instr instr =
-  match
+  match instr.instr with
+  | Set ->
+      do_set instr;
+      None
+  | Add ->
+      do_add instr;
+      None
+  | Mul ->
+      do_mul instr;
+      None
+  | Mod ->
+      do_mod instr;
+      None
+  | Rcv -> do_rcv instr
+  | Jgz -> do_jgz instr
+  | Snd ->
+      do_snd instr;
+      None
 
+let instructions = List.map aoc_input ~f:parse_instr |> Array.of_list
 
-let instructions = List.map aoc_input ~f:parse_instr
+let rec solve_p1 () =
+  let rec loop ip =
+    let instr = instructions.(ip) in
+    let res = do_instr instr in
+    match res with
+    | None -> loop ip + 1
+    | Some ret_val -> (
+        match instr.instr with
+        | Jgz -> loop (ip + ret_val)
+        | Rcv -> if ret_val = 0 then loop (ip + 1) else ret_val
+        | _ -> loop (ip + 1))
+  in
+  loop 0
+
 let result_p1 = 0
 let result_p2 = 0
