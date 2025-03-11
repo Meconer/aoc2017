ec5cbe17c9c7d58231e17e09bcc040272b5716ea
Meconer <mats.asson@gmail.com>
2025-03-11 12:43:52 +0100

    Day 19 completed

--- lib/day19/day19.ml
+++ lib/day19/day19.ml
@@ -1,6 +1,6 @@
 open Core
 
-let is_example = true
+let is_example = false
 
 let filename =
   if is_example then "lib/day19/example.txt" else "lib/day19/input.txt"
@@ -23,16 +23,70 @@ let printgrid () =
   done
 
 let start_col () =
-  fst (Option.value_exn (Array.findi grid.(0) ~f:(fun i c -> Char.equal c '|')))
+  fst (Option.value_exn (Array.findi grid.(0) ~f:(fun _ c -> Char.equal c '|')))
 
 type dir_t = Up | Down | Left | Right
 type pos_t = { r : int; c : int }
+type state_t = { pos : pos_t; dir : dir_t }
 
-let solve_p1 () = 
-  let pos = {r=0; c= start_col ()} in
-  let state = (pos, Down) in
-  let rec loop acc state = 
-     
+let get_ch pos = grid.(pos.r).(pos.c)
 
-let result_p1 = 0
-let result_p2 = 0
+let move pos dir =
+  match dir with
+  | Up -> { r = pos.r - 1; c = pos.c }
+  | Down -> { r = pos.r + 1; c = pos.c }
+  | Left -> { r = pos.r; c = pos.c - 1 }
+  | Right -> { r = pos.r; c = pos.c + 1 }
+
+let turn pos dir =
+  match dir with
+  | Up ->
+      let left = move pos Left in
+      let right = move pos Right in
+      if Char.equal (get_ch left) ' ' then (right, Right) else (left, Left)
+  | Down ->
+      let left = move pos Right in
+      let right = move pos Left in
+      if Char.equal (get_ch left) ' ' then (right, Left) else (left, Right)
+  | Left ->
+      let left = move pos Down in
+      let right = move pos Up in
+      if Char.equal (get_ch left) ' ' then (right, Up) else (left, Down)
+  | Right ->
+      let left = move pos Up in
+      let right = move pos Down in
+      if Char.equal (get_ch left) ' ' then (right, Down) else (left, Up)
+
+let string_of_dir dir =
+  match dir with
+  | Up -> "Up"
+  | Down -> "Down"
+  | Left -> "Left"
+  | Right -> "Right"
+
+let print_state state =
+  Printf.printf "Pos: r-%d c-%d   |  Dir: %s\n" state.pos.r state.pos.c
+    (string_of_dir state.dir)
+
+let solve_p1 () =
+  let state = { pos = { r = 0; c = start_col () }; dir = Down } in
+  let step_count = ref 0 in
+
+  let rec loop acc state =
+    (* print_state state; *)
+    step_count := !step_count + 1;
+    let ch = get_ch state.pos in
+    match (state.dir, ch) with
+    | _, ' ' -> (String.of_list (List.rev acc), !step_count - 1)
+    | dir, '|' -> loop acc { state with pos = move state.pos dir }
+    | dir, '-' -> loop acc { state with pos = move state.pos dir }
+    | dir, '+' ->
+        let new_pos, new_dir = turn state.pos dir in
+        loop acc { pos = new_pos; dir = new_dir }
+    | dir, ch when Char.is_alpha ch ->
+        loop (ch :: acc) { state with pos = move state.pos dir }
+    | _, _ -> failwith "Wtf?"
+  in
+  loop [] state
+
+let result_p1, result_p2 = solve_p1 ()
