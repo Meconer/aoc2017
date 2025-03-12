10a5d2b1b3aa0dee9e0d5605ce99e8be7c584bb3
Meconer <mats.asson@gmail.com>
2025-03-12 09:30:52 +0100

    wip

--- lib/day20/day20.ml
+++ lib/day20/day20.ml
@@ -8,6 +8,8 @@ let filename =
 type vector_t = { x : int; y : int; z : int }
 type particle_t = { pos : vector_t; vel : vector_t; acc : vector_t }
 
+let vec_eq vec1 vec2 = vec1.x = vec2.x && vec1.y = vec2.y && vec1.z = vec2.z
+
 let particle_of_line line =
   Scanf.sscanf line "p=< %d,%d,%d>, v=< %d,%d,%d>, a=< %d,%d,%d>"
     (fun px py pz vx vy vz ax ay az ->
@@ -18,14 +20,40 @@ let particle_of_line line =
       })
 
 let aoc_input = In_channel.read_lines filename
-let manh_dist p1 p2 = abs (p1.x - p2.x) + abs (p1.y - p2.y) + abs (p1.z - p2.z)
-
+let manh part = abs part.x + abs part.y + abs part.z
 let particles = List.map aoc_input ~f:particle_of_line
 
 let tick particles =
-  List.map particles ~f:(fun p -> let vel = {x= p.vel.x + p.acc.x; y=p.vel.y + p.acc.y; z=p.vel.z + p.acc.z} in
-  let pos = {x= p.pos.x + vel.x; y= p.pos.y + vel.y; z=p.pos.z + vel.z} in
-  {}
-  )
+  List.map particles ~f:(fun p ->
+      let vel =
+        { x = p.vel.x + p.acc.x; y = p.vel.y + p.acc.y; z = p.vel.z + p.acc.z }
+      in
+      let pos =
+        { x = p.pos.x + vel.x; y = p.pos.y + vel.y; z = p.pos.z + vel.z }
+      in
+      { pos; vel; acc = p.acc })
 
-let result_p1, result_p2 = (0, 0)
+let get_lowest_acc particles =
+  let accels = List.mapi particles ~f:(fun i part -> (i, manh part.acc)) in
+  let sorted =
+    List.sort accels ~compare:(fun a b -> Int.compare (snd a) (snd b))
+  in
+  fst (List.hd_exn sorted)
+
+let result_p1 = get_lowest_acc particles
+
+let remove_colliding particles =
+  let rec loop acc particles =
+    match particles with
+    | [] -> acc
+    | hd :: rest ->
+        if List.exists rest ~f:(fun part -> vec_eq hd.pos part.pos) then
+          loop acc (List.filter rest ~f:(fun part -> vec_eq part.pos hd.pos))
+        else loop (hd :: acc) rest
+  in
+  loop [] particles
+
+(* let solve_p2 particles =
+  let rec loop parts =
+     *)
+let result_p2 = 0
