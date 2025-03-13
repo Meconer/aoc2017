8b9baa665426a3537d7ab9cd2176f5b80ea222a7
Meconer <mats.asson@gmail.com>
2025-03-13 09:27:01 +0100

    wip

--- lib/day21/day21.ml
+++ lib/day21/day21.ml
@@ -1,11 +1,18 @@
 open Core
 
-let is_example = false
+let is_example = true
 
 let filename =
   if is_example then "lib/day21/example.txt" else "lib/day21/input.txt"
 
 let aoc_input = In_channel.read_lines filename
+let start_pattern = [| [| 0; 1; 0 |]; [| 0; 0; 1 |]; [| 1; 1; 1 |] |]
+
+let print_pattern pattern =
+  Array.iter pattern ~f:(fun line ->
+      Array.iter line ~f:(fun px ->
+          Printf.printf "%c" (if px = 1 then '#' else '.'));
+      Printf.printf "\n")
 
 let parse_line line =
   let input_pattern, output_pattern =
