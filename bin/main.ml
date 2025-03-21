ddcc5637dfe3a9f2189ef564245519b1962f8e24
Meconer <mats.asson@gmail.com>
2025-03-21 15:10:11 +0100

    Day 22 started

--- bin/main.ml
+++ bin/main.ml
@@ -38,6 +38,8 @@ let () =
      Printf.printf "Day19 part 1 : %s\n" Day19.result_p1;
      Printf.printf "Day19 part 2 : %d\n" Day19.result_p2;
      Printf.printf "Day20 part 1 : %d\n" Day20.result_p1;
-     Printf.printf "Day20 part 2 : %d\n" Day20.result_p2; *)
-  Printf.printf "Day21 part 1 : %d\n" Day21.result_p1;
-  Printf.printf "Day21 part 2 : %d\n" Day21.result_p2
+     Printf.printf "Day20 part 2 : %d\n" Day20.result_p2; 
+     Printf.printf "Day21 part 1 : %d\n" Day21.result_p1;
+     Printf.printf "Day21 part 2 : %d\n" Day21.result_p2;*)
+  Printf.printf "Day22 part 1 : %d\n" Day22.result_p1;
+  Printf.printf "Day22 part 2 : %d\n" Day22.result_p2
---
+++ lib/day22/day22.ml
@@ -0,0 +1,27 @@
+open Core
+
+let is_example = true
+
+let filename =
+  if is_example then "lib/day22/example.txt" else "lib/day22/input.txt"
+
+module Pos = struct
+  module T = struct
+    type t = { x : int; y : int }
+
+    let compare t1 t2 =
+      let cmp = Int.compare t1.x t2.x in
+      if cmp <> 0 then cmp else Int.compare t1.y t2.y
+
+    let sexp_of_t t : Sexp.t = List [ Int.sexp_of_t t.x; Int.sexp_of_t t.y ]
+    let hash t = Hashtbl.hash (t.x, t.y)
+  end
+
+  include T
+  include Comparator.Make (T)
+end
+
+let aoc_input = In_channel.read_lines filename
+let viruses = Set.empty (module Pos)
+let result_p1 = 0
+let result_p2 = 0
