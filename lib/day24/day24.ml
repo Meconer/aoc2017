713956c73cb22ec71f475703b273fe5a81e29c4e
Meconer <mats.asson@gmail.com>
2025-03-27 18:06:06 +0100

    wip

--- lib/day24/day24.ml
+++ lib/day24/day24.ml
@@ -6,5 +6,14 @@ let filename =
   if is_example then "lib/day24/example.txt" else "lib/day24/input.txt"
 
 let aoc_input = In_channel.read_lines filename
+
+let parse_line line =
+  let parts = String.split line ~on:'/' in
+  (List.hd_exn parts, List.nth_exn parts 1)
+
+let solve_p1 () =
+  let comps = List.map aoc_input ~f:parse_line in
+  comps
+
 let result_p1 = 0
 let result_p2 = 0
